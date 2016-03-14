beeb3510e5fecb484601f4571bce321514552ba2
Lunga Sizani <lsizani@gmail.com>
2016-03-14 23:18:35 +0200

    Import interface for BioRad TC20 cell counter

--- bika/lims/exportimport/instruments/__init__.py
+++ bika/lims/exportimport/instruments/__init__.py
@@ -1,41 +1,62 @@
 import sys
 import inspect
 
-from generic import xml
+#from generic import xml
 from agilent.masshunter import quantitative
 from foss.fiastar import fiastar
 from foss.winescan import auto
 from foss.winescan import ft120
 from thermoscientific.gallery import Ts9861x
 from thermoscientific.arena import xt20
+from thermoscientific.multiskan import go
 from panalytical.omnia import axios_xrf
 from alere.pima import beads, cd4
 from lifetechnologies.qubit import qubit
 from biodrop.ulite import ulite
 from tescan.tima import tima
-from sysmex.xs import i500
+from sysmex.xs import i500, i1000
+from beckmancoulter.access import model2
+from rochecobas.taqman import model48
+from sealanalytical.aq2 import aq2
+from horiba.jobinyvon import icp
+from abaxis.vetscan import vs2
+from scilvet.abc import plus
+from eltra.cs import cs2000
+from rigaku.supermini import wxrf
+from biorad.tc20 import tc20 #Lunga
+from myself import myinstrument
 
-__all__ = ['generic.xml',
+__all__ = ['abaxis.vetscan.vs2',
            'agilent.masshunter.quantitative',
+           'alere.pima.beads',
+           'alere.pima.cd4',
+           'beckmancoulter.access.model2',
+           'biodrop.ulite.ulite',
+           'biorad.tc20.tc20', #Lunga
+           'eltra.cs.cs2000',
            'foss.fiastar.fiastar',
            'foss.winescan.auto',
            'foss.winescan.ft120',
-           'thermoscientific.gallery.Ts9861x',
+           #'generic.xml',
+           'horiba.jobinyvon.icp',
+           'rigaku.supermini.wxrf',
+           'rochecobas.taqman.model48',
            'thermoscientific.arena.xt20',
+           'thermoscientific.gallery.Ts9861x',
            'panalytical.omnia.axios_xrf',
-           'alere.pima.beads',
-           'alere.pima.cd4',
            'lifetechnologies.qubit.qubit',
-           'biodrop.ulite.ulite',
+           'sysmex.xs.i500',
+           'sysmex.xs.i1000',
+           'scilvet.abc.plus',
+           'sealanalytical.aq2.aq2',
            'tescan.tima.tima',
-           'sysmex.xs.i500'
+           'thermoscientific.multiskan.go',
+           'myself.myinstrument',
            ]
-
 
 def getExim(exim_id):
     currmodule = sys.modules[__name__]
     members = [obj for name, obj in inspect.getmembers(currmodule) \
                if hasattr(obj, '__name__') \
                and obj.__name__.endswith(exim_id)]
-
-    return members[0]
+    return members[0] if len(members)>0 else None
