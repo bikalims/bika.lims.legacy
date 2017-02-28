0391e554fc95a23327cd8abb461a4d02b6b6050d
Lunga Baliwe <lunga001@gmail.com>
2017-02-28 17:32:22 +0200

    Fixed broken validator

--- bika/lims/browser/widgets/durationwidget.py
+++ bika/lims/browser/widgets/durationwidget.py
@@ -4,31 +4,24 @@
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
 
 from AccessControl import ClassSecurityInfo
-from Products.Archetypes.Widget import TypesWidget
+from Products.ATExtensions.widget import RecordWidget
 from Products.Archetypes.Registry import registerWidget
 import datetime
 
-class DurationWidget(TypesWidget):
+class DurationWidget(RecordWidget):
     security = ClassSecurityInfo()
-    _properties = TypesWidget._properties.copy()
+    _properties = RecordWidget._properties.copy()
     _properties.update({
         'macro': "bika_widgets/durationwidget",
     })
 
     def process_form(self, instance, field, form, empty_marker=None,
                      emptyReturnsMarker=False, validating=True):
-        """Basic impl for form processing in a widget"""
-        empty_marker = {} #This is a hack
-        # a poor workaround for Plone repeating itself.
-        # XXX this is important XXX
-        key = field.getName() + '_value'
-        if key in instance.REQUEST:
-            return instance.REQUEST[key], {}
         value = form.get(field.getName(), empty_marker)
         if value is empty_marker:
-            return empty_marker, {}
-        if emptyReturnsMarker and value == '':
-            return empty_marker
+            value = {}
+        if emptyReturnsMarker and not value:
+            value = {}
         return value, {}
 
 registerWidget(DurationWidget,
