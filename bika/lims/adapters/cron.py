e94520e9a30a3902826536269dc60b7efac066a2
Lunga Baliwe <lunga001@gmail.com>
2017-03-09 11:31:56 +0200

    LIMS-2392- Cron job for expiring Current Reference Samples that have expired dates

--- bika/lims/adapters/cron.py
+++ bika/lims/adapters/cron.py
@@ -1,10 +1,18 @@
 from collective.cron import crontab
 from datetime import datetime
+from plone import api
 class ExpireReferenceSamples(crontab.Runner):
     def run(self):
-        print '******************************************************'
-        bc = api.portal.get_tool('bika_catalog')
-        query = {'portal_type': 'ReferenceSample',
-                'getExpiryDate': {'query': datetime.today(), 'range': 'max'}}
-        brains = bc(query)
-        print '******************************************************'
+        try:
+            workflow = api.portal.get_tool('portal_workflow')
+            bc = api.portal.get_tool('bika_catalog')
+            query = {'portal_type': 'ReferenceSample',
+                     'getExpiryDate': {'query': datetime.today(), 'range': 'max'},
+                     'review_state': 'current',
+                    }
+            brains = bc(query)
+            for brain in brains:
+                workflow.doActionFor(brain.getObject(), 'expire')
+
+        except Exception, e:
+            raise
