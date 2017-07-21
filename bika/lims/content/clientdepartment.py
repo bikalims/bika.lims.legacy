2a4e52016534c63be6b4680e6f036f995032cbe7
Mike Metcalfe <mike@webtide.co.za>
2017-07-21 06:37:01 +0200

    Moved multi-catalog idexing functionality from new dexterity classes to an eventandler

--- bika/lims/content/clientdepartment.py
+++ bika/lims/content/clientdepartment.py
@@ -35,45 +35,6 @@ class ClientDepartment(Item):
         return [getToolByName(self, 'portal_catalog'),
                 getToolByName(self, 'bika_setup_catalog')]
 
-    def indexObject(self):
-        ''' index an object on all registered catalogs '''
-        for c in self._catalogs():
-            c.catalog_object(self)
-
-    def unindexObject(self):
-        ''' remove an object from all registered catalogs '''
-        path = '/'.join(self.getPhysicalPath())
-        for c in self._catalogs():
-            c.uncatalog_object(path)
-
-    def reindexObjectSecurity(self, skip_self=False):
-        ''' reindex only security information on catalogs '''
-        path = '/'.join(self.getPhysicalPath())
-        for c in self._catalogs():
-            for brain in c.unrestrictedSearchResults(path=path):
-                brain_path = brain.getPath()
-                if brain_path == path and skip_self:
-                    continue
-                # Get the object
-                ob = brain._unrestrictedGetObject()
-
-                # Recatalog with the same catalog uid.
-                # _cmf_security_indexes in CMFCatalogAware
-                c.reindexObject(ob,
-                                idxs=self._cmf_security_indexes,
-                                update_metadata=0,
-                                uid=brain_path)
-
-    def reindexObject(self, idxs=[]):
-        ''' reindex object '''
-        if idxs == []:
-            # Update the modification date.
-            if hasattr(aq_base(self), 'notifyModified'):
-                self.notifyModified()
-        for c in self._catalogs():
-            if c is not None:
-                c.reindexObject(self, idxs=idxs)
-
 @indexer(IClientDepartment, IBikaSetupCatalog)
 def clienttype_title_indexer(obj):
     if obj.title:
